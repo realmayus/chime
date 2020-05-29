d636832811153f79a5c7f4ee45278b82b3935948
realmayus <realmayus@gmail.com>
2020-05-30 00:29:42 +0200

    Set up basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="chime",
+    version="1.0.0",
+    description="A scalable, intuitive and easy-to-use music bot for Discord.",
+    author="realmayus",
+    author_email="realmayus@gmail.com",
+    packages=["chime"],
+    install_requires=["discord"]
+)
