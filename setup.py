f325d5b45c45dacf03d036a0ad3344ab8e937913
realmayus <realmayus@gmail.com>
2020-05-30 12:13:26 +0200

    Set up setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="chime",
     version="1.0.0",
     description="A scalable, intuitive and easy-to-use music bot for Discord.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="realmayus",
     author_email="realmayus@gmail.com",
     packages=["chime"],
-    install_requires=["discord"]
+    install_requires=["discord"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Communications",
+        "Topic :: Communications :: Conferencing",
+        "Topic :: Multimedia :: Sound/Audio :: Players"
+    ]
 )
