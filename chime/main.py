5e2309e95cbce3dc2d6a2e1122674a7379178e01
realmayus <realmayus@gmail.com>
2020-06-10 17:24:44 +0200

    debug

--- chime/main.py
+++ chime/main.py
@@ -7,7 +7,6 @@ from discord.ext import commands
 # If activated: Uses the token-dev to minimize downtime while developing
 start_dev = True
 
-logger = None
 
 version = "1.0.0"
 prefix = "*" if start_dev else "$"
