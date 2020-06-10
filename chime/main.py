9a9beb65b1230cb56f330172c0d2883b9a2d153b
realmayus <realmayus@gmail.com>
2020-06-10 15:46:08 +0200

    Fixed logger pls work now really

--- chime/main.py
+++ chime/main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 start_dev = True
 
 log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
-log_file = '../../log.txt'
+log_file = 'log.txt'
 my_handler = RotatingFileHandler(filename=log_file, mode='w', maxBytes=5 * 1024 * 1024, backupCount=2,
                                  encoding=None, delay=0)
 my_handler.setFormatter(log_formatter)
