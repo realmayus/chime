08b024cebbb551c21a40f42de47ae604ff68b375
realmayus <realmayus@gmail.com>
2020-06-13 12:09:14 +0200

    Fixed Playlists command

--- chime/cogs/PersonalPlaylistsCog.py
+++ chime/cogs/PersonalPlaylistsCog.py
@@ -9,7 +9,7 @@ from discord.ext.commands import Context
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1 import Client, DocumentReference
-from wavelink import Player, TrackPlaylist, BuildTrackError
+from wavelink import Player, TrackPlaylist, BuildTrackError, Track
 
 from chime.main import prefix
 from chime.misc.BadRequestException import BadRequestException
@@ -29,12 +29,17 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
     @commands.command()
     async def playlists(self, ctx: Context):
         """Shows a list of all your playlists"""
-        docs = self.db.collection(str(ctx.author.id)).stream()  # TODO fix this command!
-        await ctx.send(embed=StyledEmbed(title="Your playlists", description=f"Use the `{prefix}playlist` command for creating new playlists, viewing them and much more! \n\n" + "\n".join([f"•  **{playlist.id}**  ({len(playlist.to_dict()['contents'])} songs)" for playlist in docs])))
+        profile_ref: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
+        profile = profile_ref.get()
+        profile_data: dict = profile.to_dict()
+        if "playlists" in profile_data.keys() and len(profile_data["playlists"]) > 0:
+            await ctx.send(embed=StyledEmbed(title="Your playlists", description=f"use the `{prefix}playlist` commands for adding songs to a playlist, creating playlists, viewing the playlist's contents etc. \n\n" + "\n".join([f"•  **{playlist['name']}**" for playlist in profile_data['playlists']])))
+        else:
+            raise BadRequestException(f"You currently don't have any playlists. Create one with `{prefix}playlist create <name>`.")
 
     @commands.command(usage="playlist [action] [playlist_name]")
     async def playlist(self, ctx: Context, action: str, playlist: str, *, additional_args=None):
-        """Provide the argument 'create' to create a playlist. Provide the argument 'show' to show the playlist's contents. Provide argument 'play' to play the playlist. Provide argument 'delete' to delete playlist. Provide argument 'link' to get a link to the playlist."""
+        """Provide the argument 'create' to create a playlist. Provide 'add' and a search term/URL to add a track to a playlsit Provide the argument 'show' to show the playlist's contents. Provide argument 'play' to play the playlist. Provide argument 'delete' to delete playlist. Provide argument 'link' to get a link to the playlist."""
         if action == "create":
             if not additional_args:  # if the playlist name contained spaces, the individual parts would be in additional_args
                 profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
@@ -53,7 +58,7 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                     raise BadRequestException(f"A playlist with the name `{playlist}` exists already!")
             else:
                 raise BadRequestException("The playlist name must not contain spaces!")
-        elif action == "show":
+        elif action == "show" or action == "view":
             profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
             x = check_if_playlist_exists(profile, playlist)
             if x is not False:
@@ -110,68 +115,66 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                     raise BadRequestException(f"No playlist with the name {playlist} exists!")
             else:
                 raise BadRequestException(f"No playlist with the name {playlist} exists!")
+        elif action == "add":
+            tracks_to_add = []
 
+            async def success_callback(track_, last_msg: Message):
+                await last_msg.clear_reactions()
+                await last_msg.edit(embed=StyledEmbed(description=f"**Added** {track_} **to playlist {playlist}.**"),
+                                    delete_after=10.0)
+                tracks_to_add.append(track_)
 
+            async def success_callback_url(tracks):
+                nonlocal tracks_to_add
+                if isinstance(tracks, TrackPlaylist):
+                    tracks = tracks.tracks
+                    await ctx.send(
+                        embed=StyledEmbed(description=f"**Added** {len(tracks)} **tracks to playlist {playlist}.**"),
+                        delete_after=10.0)
+                else:
+                    try:
+                        await ctx.send(
+                            embed=StyledEmbed(description=f"**Added** {tracks[0]} **to playlist {playlist}.**"),
+                            delete_after=10.0)
+                    except TypeError:
+                        raise BadRequestException("Couldn't add this item to the queue!")
+                tracks_to_add = tracks
 
+            if not additional_args:
+                raise BadRequestException("You have to provide either a search term or a URL!")
+
+            await search_song(additional_args, ctx, self.bot, success_callback, success_callback_url)
+
+            profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
+            x = check_if_playlist_exists(profile, playlist)
+            if len(tracks_to_add) > 0:
+                if x is not False:
+                    playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
+                    if playlist_doc is not None:
+                        playlist_doc.update({"contents": firestore.ArrayUnion(
+                            [{"title": track_to_add.title, "author": track_to_add.author, "data": track_to_add.id, "url": track_to_add.uri, "id": str(uuid.uuid4()), "duration": track_to_add.duration} for
+                             track_to_add in tracks_to_add])})
+                        await ctx.message.add_reaction("<:OK:716230152643674132>")
+                    else:
+                        raise BadRequestException(f"No playlist with the name {playlist} exists!")
+                else:
+                    raise BadRequestException(f"No playlist with the name {playlist} exists!")
+            else:
+                raise BadRequestException("No track selected!")
         else:
-            raise BadRequestException("This action does not exist. Valid actions are: `create`, `show`, `play`, `delete` and `link`.")
+            raise BadRequestException("This action does not exist. Valid actions are: `create`, `add`, `show`, `play`, `delete` and `link`.")
 
     @commands.command()
     async def like(self):
         """Adds the current song to your 'Liked Songs' playlist"""
 
-
     @commands.command()
     async def dislike(self):
         """Removes the current song/the given song from your 'Liked Songs' playlist"""
 
-
-    @commands.command(usage="add [playlist] <search term>")
-    async def add(self, ctx: Context, playlist: str, *, search_term: str = None):
-        """Adds the current song or a song from a search term to the given playlist"""
-        tracks_to_add = []
-
-        async def success_callback(track_, last_msg: Message):
-            await last_msg.clear_reactions()
-            await last_msg.edit(embed=StyledEmbed(description=f"**Added** {track_} **to playlist {playlist}.**"), delete_after=10.0)
-            print(track_)
-            tracks_to_add.append(track_)
-
-        async def success_callback_url(tracks):
-            nonlocal tracks_to_add
-            if isinstance(tracks, TrackPlaylist):
-                tracks = tracks.tracks
-                await ctx.send(embed=StyledEmbed(description=f"**Added** {len(tracks)} **tracks to playlist {playlist}.**"), delete_after=10.0)
-            else:
-                try:
-                    await ctx.send(embed=StyledEmbed(description=f"**Added** {tracks[0]} **to playlist {playlist}.**"), delete_after=10.0)
-                except TypeError:
-                    raise BadRequestException("Couldn't add this item to the queue!")
-            tracks_to_add = tracks
-
-        await search_song(search_term, ctx, self.bot, success_callback, success_callback_url)
-
-        profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
-        x = check_if_playlist_exists(profile, playlist)
-        if len(tracks_to_add) > 0:
-            if x is not False:
-                playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
-                if playlist_doc is not None:
-                    playlist_doc.update({"contents": firestore.ArrayUnion([{"title": track_to_add.title, "author": track_to_add.author, "data": track_to_add.id} for track_to_add in tracks_to_add])})
-                    await ctx.message.add_reaction("<:OK:716230152643674132>")
-                else:
-                    raise BadRequestException(f"No playlist with the name {playlist} exists!")
-            else:
-                raise BadRequestException(f"No playlist with the name {playlist} exists!")
-        else:
-            raise BadRequestException("No track selected!")
-
-
-
     @commands.command(usage="remove [playlist] <search term>")
     async def remove(self, ctx: Context, playlist: str, search_term: str = None):
         """Removes the current song or a song from a search term from the given playlist"""
-
 
     def get_controller(self, value: Union[commands.Context, wavelink.Player]):
         """Return the given guild's instance of the MusicController"""
