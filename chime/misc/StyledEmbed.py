aece9dbefd0e62ede2c52c890801cee7bf687d26
realmayus <realmayus@gmail.com>
2020-07-02 13:37:30 +0200

    "Removed" embed color on the left

--- chime/misc/StyledEmbed.py
+++ chime/misc/StyledEmbed.py
@@ -4,4 +4,4 @@ import discord
 class StyledEmbed(discord.Embed):
     def __init__(self, **kwargs):
         """A styled embed."""
-        super(StyledEmbed, self).__init__(color=discord.colour.Color.from_rgb(r=255, g=197, b=84), **kwargs)
+        super(StyledEmbed, self).__init__(color=discord.colour.Color.from_rgb(r=47, g=49, b=54), **kwargs)
