1d04421fac019d9e41a3409eedb6dedddeee6bf0
realmayus <realmayus@gmail.com>
2020-09-12 11:56:46 +0200

    Added checks for *playlist command for checking if user entered the playlist name, moved *share command to *pl share

--- chime/cogs/PersonalPlaylistsCog.py
+++ chime/cogs/PersonalPlaylistsCog.py
@@ -48,7 +48,7 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                     {"name": "add", "desc": "Adds a song to the given playlist."},
                     {"name": "show | view", "desc": "Lists the songs in your playlist."},
                     {"name": "play", "desc": "Plays the playlist."},
-                    {"name": "delete", "desc": "Deletes the playlist."}
+                    {"name": "share", "desc": "Gives you a link so that you can share your beautiful playlist!"}
                 ]
              }],
         examples=[
@@ -75,6 +75,10 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
             {
                 "ex": "pl list",
                 "desc": "Lists all your playlists."
+            },
+            {
+                "ex": "pl share chillhop",
+                "desc": "Displays a sharable link for the playlist 'chillhop'."
             }
         ]
     )
@@ -92,63 +96,71 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                 raise BadRequestException("If you want spaces in your playlist's name, you have to wrap it in quotation marks!")
         elif action == "show" or action == "view":
             profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
-            x = check_if_playlist_exists(profile, playlist)
-            if x is not False:
-                playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
-                if playlist_doc is not None:
-                    playlist_data_raw = playlist_doc.get()
-                    playlist_data: dict = playlist_data_raw.to_dict()
-                    if "contents" in playlist_data.keys():
-                        contents = playlist_data["contents"]
-                        if len(contents) == 0:
-                            raise BadRequestException("Playlist is empty!")
+            if playlist is not None:
+                x = check_if_playlist_exists(profile, playlist)
+                if x is not False:
+                    playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
+                    if playlist_doc is not None:
+                        playlist_data_raw = playlist_doc.get()
+                        playlist_data: dict = playlist_data_raw.to_dict()
+                        if "contents" in playlist_data.keys():
+                            contents = playlist_data["contents"]
+                            if len(contents) == 0:
+                                raise BadRequestException("Playlist is empty!")
 
-                        embed = PagedListEmbed(f"Contents of `{playlist}`", [f"{i + 1}.  {song['title']}" for i, song in enumerate(contents)], ctx, self.bot)
-                        await embed.send(embed.get())
+                            embed = PagedListEmbed(f"Contents of `{playlist}`", [f"{i + 1}.  {song['title']}" for i, song in enumerate(contents)], ctx, self.bot)
+                            await embed.send(embed.get())
 
+                        else:
+                            raise BadRequestException(f"Could not load playlist {playlist}!")
                     else:
-                        raise BadRequestException(f"Could not load playlist {playlist}!")
+                        raise BadRequestException(f"No playlist with the name {playlist} exists!")
                 else:
                     raise BadRequestException(f"No playlist with the name {playlist} exists!")
             else:
-                raise BadRequestException(f"No playlist with the name {playlist} exists!")
+                raise BadRequestException("Please enter a playlist name!")
         elif action == "play":
             profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
-            x = check_if_playlist_exists(profile, playlist)
-            if x is not False:
-                playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
-                if playlist_doc is not None:
-                    playlist_data_raw = playlist_doc.get()
-                    playlist_data: dict = playlist_data_raw.to_dict()
-                    if "contents" in playlist_data.keys():
-                        contents = playlist_data["contents"]
-                        await self.join_channel(ctx)
-                        if len(contents) == 0:
-                            raise BadRequestException("Playlist is empty!")
+            if playlist is not None:
+                x = check_if_playlist_exists(profile, playlist)
+                if x is not False:
+                    playlist_doc: DocumentReference = self.db.collection(str(ctx.author.id)).document(x)
+                    if playlist_doc is not None:
+                        playlist_data_raw = playlist_doc.get()
+                        playlist_data: dict = playlist_data_raw.to_dict()
+                        if "contents" in playlist_data.keys():
+                            contents = playlist_data["contents"]
+                            await self.join_channel(ctx)
+                            if len(contents) == 0:
+                                raise BadRequestException("Playlist is empty!")
 
-                        index = 0
-                        failed = 0
-                        for index, song_data_raw in enumerate(contents):
-                            try:
-                                track = await self.bot.wavelink.build_track(song_data_raw["data"])
-                                controller = self.get_controller(ctx)
-                                controller.queue.append(track)
-                            except BuildTrackError:
-                                failed += 1
-                                print("Failed to reconstruct track with data " + song_data_raw["data"])
-                        await ctx.send(embed=StyledEmbed(description=f"**Added** {index + 1} **tracks to queue**."))
-                        if failed > 0:
-                            raise BadRequestException(f"**Failed to add** {failed} **track(s)**!")
+                            index = 0
+                            failed = 0
+                            for index, song_data_raw in enumerate(contents):
+                                try:
+                                    track = await self.bot.wavelink.build_track(song_data_raw["data"])
+                                    controller = self.get_controller(ctx)
+                                    controller.queue.append(track)
+                                except BuildTrackError:
+                                    failed += 1
+                                    print("Failed to reconstruct track with data " + song_data_raw["data"])
+                            await ctx.send(embed=StyledEmbed(description=f"**Added** {index + 1} **tracks to queue**."))
+                            if failed > 0:
+                                raise BadRequestException(f"**Failed to add** {failed} **track(s)**!")
+                        else:
+                            raise BadRequestException(f"Could not load playlist {playlist}!")
                     else:
-                        raise BadRequestException(f"Could not load playlist {playlist}!")
+                        raise BadRequestException(f"No playlist with the name {playlist} exists!")
                 else:
                     raise BadRequestException(f"No playlist with the name {playlist} exists!")
             else:
-                raise BadRequestException(f"No playlist with the name {playlist} exists!")
+                raise BadRequestException("Please enter a playlist name!")
         elif action == "list":
             await ctx.invoke(self.playlists)
         elif action == "add":
             tracks_to_add = []
+            if playlist is None:
+                raise BadRequestException("Please enter a playlist name!")
 
             async def success_callback(track_, last_msg: Message):
                 await last_msg.clear_reactions()
@@ -193,8 +205,24 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                     raise BadRequestException(f"No playlist with the name {playlist} exists!")
             else:
                 raise BadRequestException("No track selected!")
+        elif action == "share":
+            if playlist is None:
+                raise BadRequestException("Please enter a playlist name!")
+            profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
+            playlist_id = check_if_playlist_exists(profile, playlist)
+            if not playlist_id:
+                raise BadRequestException(f"No playlist with the name {playlist} exists!")
+
+            message = f"{ctx.author.id}:{playlist_id}:{playlist}:{ctx.author.name}"
+            message_bytes = message.encode("utf8")
+            base64_bytes = base64.b64encode(message_bytes)
+            base64_message = base64_bytes.decode("ascii")
+            await ctx.send(embed=StyledEmbed(title="Share this link",
+                                             description=f"https://chime.realmayus.xyz/view/{base64_message}"))
+        elif action == "delete":
+            raise BadRequestException("This feature has not been implemented yet.")
         else:
-            raise BadRequestException("This action does not exist. Valid actions are: `create`, `list`, `add`, `show`, `play`, `delete` and `link`.")
+            raise BadRequestException("This action does not exist. Valid actions are: `create`, `list`, `add`, `show`, `play`, `delete` and `share`.")
 
     def create_playlist(self, profile, userID, playlist):
         playlist_id = str(uuid.uuid4())
@@ -228,7 +256,6 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
                   "url": current_track.uri, "id": str(uuid.uuid4()), "duration": current_track.duration}])})
             await ctx.message.add_reaction("<:ok:746377326245445653>")
 
-
     @commands.command()
     async def unlike(self, ctx):
         """Removes the current song from your 'Liked' playlist"""
@@ -246,20 +273,6 @@ class PersonalPlaylistsCog(commands.Cog, name="Personal Playlists"):
             contents = [item for item in contents if item["url"] != current_track.uri]
             playlist_doc_ref.update({"contents": contents})
             await ctx.message.add_reaction("<:ok:746377326245445653>")
-
-    @commands.command()
-    async def share(self, ctx, *, playlist):
-        """Shows a shareable link so that others can see and clone your beautiful playlist!"""
-        profile: DocumentReference = self.db.collection(str(ctx.author.id)).document("profile")
-        playlist_id = check_if_playlist_exists(profile, playlist)
-        if not playlist_id:
-            raise BadRequestException(f"No playlist with the name {playlist} exists!")
-
-        message = f"{ctx.author.id}:{playlist_id}:{playlist}:{ctx.author.name}"
-        message_bytes = message.encode("utf8")
-        base64_bytes = base64.b64encode(message_bytes)
-        base64_message = base64_bytes.decode("ascii")
-        await ctx.send(embed=StyledEmbed(title="Share this link", description=f"https://chime.realmayus.xyz/view/{base64_message}"))
 
     def get_controller(self, value: Union[commands.Context, wavelink.Player]):
         """Return the given guild's instance of the MusicController"""
