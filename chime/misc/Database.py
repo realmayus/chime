fc29939589ba3436a3d1f1e169a5d7cf5870c80a
realmayus <realmayus@gmail.com>
2020-10-23 15:05:27 +0200

    Moved everything over to Database.py, created new functions there

--- chime/misc/Database.py
+++ chime/misc/Database.py
@@ -37,12 +37,36 @@ class Database:
         except google.api_core.exceptions.NotFound:
             profile.set({"playlists": []}, merge=True)
             profile.update({"playlists": firestore.ArrayUnion([{"name": name, "ref": playlist_id}])})
+        return playlist_doc
 
+    def create_if_non_existant(self, user_id: int, name: str):
+        profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
+        if check_if_playlist_exists(profile, name):
+            return
+        playlist_id = str(uuid.uuid4())
+        playlist_doc: DocumentReference = self.db.collection(str(user_id)).document(playlist_id)
+        playlist_doc.set({"contents": []})
+        try:
+            profile.update({"playlists": firestore.ArrayUnion([{"name": name, "ref": playlist_id}])})
+        except google.api_core.exceptions.NotFound:
+            profile.set({"playlists": []}, merge=True)
+            profile.update({"playlists": firestore.ArrayUnion([{"name": name, "ref": playlist_id}])})
+        return playlist_doc
+
+
+    def get_playlist_doc(self, user_id: int, name: str) -> DocumentReference:
+        profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
+        playlist_doc: DocumentReference = self.db.collection(str(user_id)).document(check_if_playlist_exists(profile, name))
+        playlist_data = playlist_doc.get().to_dict()
+        if playlist_doc is False:
+            raise DatabaseExceptions.PlaylistDoesNotExistException()
+        if "contents" not in playlist_data.keys():
+            raise DatabaseExceptions.PlaylistCorruptedException(name, user_id)
         return playlist_doc
 
     def get_playlist_contents(self, user_id: int, name: str) -> dict:
         profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
-        playlist_doc: DocumentReference = check_if_playlist_exists(profile, name)
+        playlist_doc: DocumentReference = self.db.collection(str(user_id)).document(check_if_playlist_exists(profile, name))
         playlist_data = playlist_doc.get().to_dict()
         if playlist_doc is False:
             raise DatabaseExceptions.PlaylistDoesNotExistException()
@@ -52,12 +76,37 @@ class Database:
 
     def add_to_playlist(self, user_id: int, name: str, tracks: List[Track]):
         profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
-        playlist_doc: DocumentReference = check_if_playlist_exists(profile, name)
+        playlist_doc: DocumentReference = self.db.collection(str(user_id)).document(check_if_playlist_exists(profile, name))
         if playlist_doc is False:
             raise DatabaseExceptions.PlaylistDoesNotExistException()
         playlist_doc.update({"contents": firestore.ArrayUnion(
                             [{"title": track_to_add.title, "author": track_to_add.author, "data": track_to_add.id, "url": track_to_add.uri, "id": str(uuid.uuid4()), "duration": track_to_add.duration} for
                              track_to_add in tracks])})
+
+    def remove_from_playlist(self, user_id: int, name: str, tracks: List[Track]):
+        profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
+        playlist_doc: DocumentReference = self.db.collection(str(user_id)).document(check_if_playlist_exists(profile, name))
+        if playlist_doc is False:
+            raise DatabaseExceptions.PlaylistDoesNotExistException()
+        existing_contents = self.get_playlist_contents(user_id, name)
+        new_contents = existing_contents
+        for track in tracks:
+            new_contents = [item for item in new_contents if item["url"] != track.uri]
+        playlist_doc.set({"contents": new_contents}, False)
+
+    def raise_if_not_exists(self, user_id: int, name: str) -> str:
+        profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
+        playlist_id = check_if_playlist_exists(profile, name)
+        if playlist_id is False:
+            raise DatabaseExceptions.PlaylistDoesNotExistException()
+        return playlist_id
+
+    def check_if_exists(self, user_id: int, name: str) -> bool:
+        profile: DocumentReference = self.db.collection(str(user_id)).document("profile")
+        playlist_id = check_if_playlist_exists(profile, name)
+        if playlist_id is not False:
+            return playlist_id
+        return False
 
 
 
@@ -65,7 +114,7 @@ class DatabaseExceptions:
     class NoProfileFoundException(Exception):
         def __init__(self):
             """Gets raised when no profile can be found for the given user id."""
-            super(DatabaseExceptions.NoProfile, self).__init__("No profile found for the given user id.")
+            super(DatabaseExceptions.NoProfileFoundException, self).__init__("No profile found for the given user id.")
             self.text = "No profile found for the given user id."
 
     class ProfileCorruptedException(Exception):
@@ -93,9 +142,9 @@ class DatabaseExceptions:
             self.text = "The given playlist can't be found."
 
     class PlaylistCorruptedException(Exception):
-        def __init__(self, name: str, user_id: str):
+        def __init__(self, name: str, user_id: int):
             """Gets raised when the given playlist seems to be corrupted, i.e. important keys are missing."""
-            super(DatabaseExceptions.ProfileCorruptedException, self).__init__(f"The given playlist (uid: {user_id}, name: {name}) seems to be corrupted, i.e. important keys are missing.")
+            super(DatabaseExceptions.PlaylistCorruptedException, self).__init__(f"The given playlist (uid: {user_id}, name: {name}) seems to be corrupted, i.e. important keys are missing.")
             self.text = f"The given playlist (uid: {user_id}, name: {name}) seems to be corrupted, i.e. important keys are missing."
 
 
